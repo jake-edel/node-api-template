037cd8392ca93087be7af8c1cc98d0d9ee68e430
Jakob Edelstein <jakobedelstein@gmail.com>
2024-03-30 08:50:31 -0500

    edits env variables

--- src/services/db.js
+++ src/services/db.js
@@ -2,7 +2,7 @@ import mysql from 'mysql'
 
 const conn = mysql.createConnection({
     host: '127.0.0.1',
-    user: process.env.USER,
+    user: process.env.DBUSER,
     password: process.env.PASSWORD,
     port: process.env.PORT,
     database: process.env.DATABASE,
