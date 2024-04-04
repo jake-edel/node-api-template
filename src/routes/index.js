06750b29d802e16a7920cbd5d4566a93f236687e
Jakob Edelstein <jakobedelstein@gmail.com>
2024-04-04 05:50:08 -0500

    update routes to point to test table

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,17 @@ import controllers from '../controllers/index.js'
 
 const router = express.Router()
 
+router.route('/')
+
 router
-    .route('/')
+    .route('/testTable')
     .get(controllers.getAllRows)
     .post(controllers.createRow)
     .delete(controllers.deleteRow)
     .put(controllers.updateRow)
 
 router
-    .route('/:id')
+    .route('/testTable/:id')
     .get(controllers.readRow)
 
 export default router
